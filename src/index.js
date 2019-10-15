50231dccf72fd3bef3d4a09e91b75cd26edfe274
rabingithub <rabinofficial09@gmail.com>
2019-10-15 15:16:30 +0545

    My first Commit

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
+
+
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faStroopwafel } from '@fortawesome/free-solid-svg-icons'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+
+import App from './components'
+ import './assets/css/index.css'
+
+library.add(fab, faStroopwafel)
+
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
@@ -10,3 +22,6 @@ ReactDOM.render(<App />, document.getElementById('root'));
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+
+
